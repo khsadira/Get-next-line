8c54cefece9354a660e60895f0966d6ef9804158
Khan SADIRAC <khsadira@e3r7p10.42.fr>
2018-02-20 15:22:06 +0100

    gnl file .c

--- get_next_line.c
+++ get_next_line.c
@@ -6,48 +6,73 @@
 /*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/09 16:32:02 by schakor           #+#    #+#             */
-/*   Updated: 2018/01/09 17:09:09 by schakor          ###   ########.fr       */
+/*   Updated: 2018/02/20 15:05:42 by khsadira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+static char *ft_switch(char *str, char *line)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	while (str[i] == '\n')
+		i++;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			(line)[j++] = str[i++];
+	}
+	line[j] = '\0';
+	return (str);
+}
+
+static char	ft_end_of_line(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (str[i]);
+		i++;
+	}
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	int			rd;
-	char		*buf;
-	static int	i;
+	static char	*buf;
+	char		*str;
+	int		i;
 	int		j;
 
-	if (!(buf = (char *)malloc(sizeof(*buf) * BUFF_SIZE)))
+	if (!(buf = (char *)malloc(sizeof(*buf) * BUFF_SIZE + 1)))
 		return (-1);
-	i = 0;
-	j = i;
-	if ((rd = read(fd, buf, BUFF_SIZE)) != -1)
+	if (!(line = (char **)malloc(sizeof(char *) * BUFF_SIZE + 1)))
+		return (-1);
+	rd = read(fd, str, BUFF_SIZE);
+	str[rd] = '\0';
+	if (ft_end_of_line(str) == '\n')
 	{
-		buf[rd] = '\0';
-		if (buf[i] == '\0')
-			return (0);
-		while (buf[i] != '\n')
-			++i;
-		while (j < i)
-		{
-			(*line)[j] = buf[j];
-			j++;
-		}
-		(*line)[j] = buf[j];
-		i++;
-		return (1);
+		printf("salut\n");
+		ft_putendl(ft_switch(str, &**line));
+		return (0)
 	}
-	return (-1);
+	while ((rd = read(fd, str, BUFF_SIZE)))
+	{
+		str[rd] = '\0';
+		buf = ft_strcat(buf, str);
+	}
 }
 
 int main(int ac, char **av)
 {
-	if (ac != 2)
-		return (0);
-	get_next_line(0, &av[1]);
-	printf("%s\n", av[1]);
-	return (0);
+	char **line;
+
+	
 }
