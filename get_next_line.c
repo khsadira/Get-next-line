e61bb7c4ceb79fc352859cd867dbae1a3f8bafcb
Simon CHAKOR <schakor@e3r7p12.42.fr>
2018-02-20 17:15:27 +0100

    gnl fiable

--- get_next_line.c
+++ get_next_line.c
@@ -5,74 +5,98 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/09 16:32:02 by schakor           #+#    #+#             */
-/*   Updated: 2018/02/20 15:05:42 by khsadira         ###   ########.fr       */
+/*   Created: 2018/02/20 15:51:33 by schakor           #+#    #+#             */
+/*   Updated: 2018/02/20 17:13:40 by schakor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-static char *ft_switch(char *str, char *line)
+int		ft_backslashn_chr(char *str)
 {
-	int	i;
-	int	j;
-	
+	int		i;
+
 	i = 0;
-	while (str[i] == '\n')
-		i++;
 	while (str[i])
 	{
 		if (str[i] == '\n')
-			(line)[j++] = str[i++];
+			return (i);
+		++i;
 	}
-	line[j] = '\0';
-	return (str);
+	return (-1);
 }
 
-static char	ft_end_of_line(char *str)
+char	*my_realloc(char *str, size_t size)
 {
-	int	i;
+	char 	*ret;
+	size_t	len;
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\n')
-			return (str[i]);
-		i++;
-	}
+	len = ft_strlen(str);
+	if (len > size || !str)
+		return (NULL);
+	if (!(ret = (char *)malloc(sizeof(*ret) * (size + 1))))
+		return (NULL);
+	ft_strcpy(ret, str);
+	free(str);
+	return (ret);
+}
+
+char	*ft_strfjoin(char *s1, char const *s2)
+{
+	char		*ret;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	if (!(ret = (char *)malloc(sizeof(*ret) * (ft_strlen(s1) + ft_strlen(s2)
+						+ 1))))
+		return (NULL);
+	ft_strcpy(ret, s1);
+	ft_strcat(ret, s2);
+	free(s1);
+	return (ret);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	int			rd;
-	static char	*buf;
-	char		*str;
-	int		i;
-	int		j;
+	static char		*over = NULL;
+	char			buf[BUFF_SIZE + 1];
+	char			*str;
+	int				rd;
+	int				i;
+	int				size;
 
-	if (!(buf = (char *)malloc(sizeof(*buf) * BUFF_SIZE + 1)))
-		return (-1);
-	if (!(line = (char **)malloc(sizeof(char *) * BUFF_SIZE + 1)))
-		return (-1);
-	rd = read(fd, str, BUFF_SIZE);
-	str[rd] = '\0';
-	if (ft_end_of_line(str) == '\n')
+	size = BUFF_SIZE;
+	if (fd == -1)
 	{
-		printf("salut\n");
-		ft_putendl(ft_switch(str, &**line));
-		return (0)
 	}
-	while ((rd = read(fd, str, BUFF_SIZE)))
+	while ((rd = read(fd, buf, BUFF_SIZE)))
 	{
-		str[rd] = '\0';
-		buf = ft_strcat(buf, str);
+		buf[rd] = '\0';
+		if ((i = ft_backslashn_chr(buf)) != -1)
+		{
+			size -= i;
+			*line = my_realloc(*line, size); 
+			*line = mettre dans line en + les i premier car de buf(*line, buf);
+			if (over)
+				free(over);
+			over = ft_strsub(buf, i, (BUFF_SIZE + 1) - i);
+		}
+		*line = my_realloc(*line, size += BUFF_SIZE); 
+		*line = ft_strfjoin(*line, buf);
 	}
+	return (0);
 }
 
-int main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	char **line;
+	char	*str;
+	int		fd;
 
-	
+	str = NULL;
+	fd = open(av[1], O_RDONLY);
+	while (get_next_line(fd, &str) > 0)
+	{
+		ft_putendl(str);
+	}
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,21 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: schakor <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/09 16:26:40 by schakor           #+#    #+#             */
-/*   Updated: 2018/01/09 16:54:47 by schakor          ###   ########.fr       */
+/*   Created: 2018/02/20 15:50:10 by schakor           #+#    #+#             */
+/*   Updated: 2018/02/20 16:24:28 by schakor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
 # include "libft/libft.h"
-# include "sys/types.h"
-# include "sys/uio.h"
-# include "unistd.h"
-# include "stdlib.h"
-# define BUFF_SIZE 496
-int		get_next_line(const int fd, char **line);
+# define BUFF_SIZE 32
 
+
+int		get_next_line(const int fd, char **line);
 
 #endif
